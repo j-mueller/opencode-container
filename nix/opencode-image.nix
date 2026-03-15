c8798cfb808825a33a3f8b758bd24342f7a5cdf5
Jann Müller <j-mueller@noreply.users.github.com>
2026-03-15 11:03:15 +0100

    Restore skopeo patch and update release docs

--- nix/opencode-image.nix
+++ nix/opencode-image.nix
@@ -8,6 +8,80 @@ let
   # Import the flake input source path directly to avoid import-from-derivation.
   n2cLib = import n2c.outPath { pkgs = n2cCompatPkgs; };
 
+  skopeoContainersImagePatch = pkgs.fetchpatch2 {
+    url = "https://github.com/nlewo/container-libs/commit/21b053ac62f3137de42585611953e923577d0e10.patch";
+    sha256 = "sha256-pfwQh7FKWHY/xVAGMSvnjMOmkpMo9NG2HFZqhqZ1VN0=";
+    postFetch = ''
+      sed -i \
+        -e '/^index /d' \
+        -e '/^similarity index /d' \
+        -e '/^dissimilarity index /d' \
+        $out
+    '';
+  };
+
+  patchedSkopeoN2C = n2cLib.skopeo-nix2container.overrideAttrs (old: {
+    preBuild = ''
+      patch_file="$(mktemp)"
+      cp ${skopeoContainersImagePatch} "$patch_file"
+      sed -i 's#go.podman.io/image/v5#github.com/containers/image/v5#g' "$patch_file"
+      cat "$patch_file"
+      mkdir -p vendor/github.com/nlewo/nix2container/
+      cp -r ${n2cLib.nix2container-bin.src}/* vendor/github.com/nlewo/nix2container/
+      chmod -R u+w vendor/github.com/nlewo/nix2container/nix
+      sed -i 's#go.podman.io/image/v5#github.com/containers/image/v5#g' \
+        vendor/github.com/nlewo/nix2container/nix/image.go
+      cd vendor/github.com/containers/image/v5
+      mkdir nix/
+      touch nix/transport.go
+      cat "$patch_file" | patch -p2
+      cd -
+
+      # Go checks packages in the vendor directory are declared in the modules.txt file.
+      awk '
+        $0 ~ /^# github.com\/containers\/image\/v5 / { print; in_mod=1; next }
+        in_mod && /^## / { print; print "github.com/containers/image/v5/nix"; in_mod=0; next }
+        { print }
+      ' vendor/modules.txt > vendor/modules.txt.tmp
+      mv vendor/modules.txt.tmp vendor/modules.txt
+
+      echo '# github.com/nlewo/nix2container v1.0.0' >> vendor/modules.txt
+      echo '## explicit; go 1.13' >> vendor/modules.txt
+      echo github.com/nlewo/nix2container/nix >> vendor/modules.txt
+      echo github.com/nlewo/nix2container/types >> vendor/modules.txt
+      # All packages declared in the modules.txt file must also be required by the go.mod file.
+      echo 'require (' >> go.mod
+      echo '  github.com/nlewo/nix2container v1.0.0' >> go.mod
+      echo ')' >> go.mod
+    '';
+  });
+
+  writeSkopeoApplication = name: text: pkgs.writeShellApplication {
+    inherit name text;
+    runtimeInputs = [ pkgs.jq patchedSkopeoN2C ];
+    excludeShellChecks = [ "SC2068" ];
+  };
+
+  copyToDockerDaemon = image: writeSkopeoApplication "copy-to-docker-daemon" ''
+    echo "Copy to Docker daemon image ${image.imageName}:${image.imageTag}"
+    skopeo --insecure-policy copy nix:${image} docker-daemon:${image.imageName}:${image.imageTag} "$@"
+  '';
+
+  copyToRegistry = image: writeSkopeoApplication "copy-to-registry" ''
+    echo "Copy to Docker registry image ${image.imageName}:${image.imageTag}"
+    skopeo --insecure-policy copy nix:${image} docker://${image.imageName}:${image.imageTag} "$@"
+  '';
+
+  copyToPodman = image: writeSkopeoApplication "copy-to-podman" ''
+    echo "Copy to podman image ${image.imageName}:${image.imageTag}"
+    skopeo --insecure-policy copy nix:${image} containers-storage:${image.imageName}:${image.imageTag} "$@"
+  '';
+
+  copyTo = image: writeSkopeoApplication "copy-to" ''
+    echo "Running skopeo --insecure-policy copy nix:${image}" "$@"
+    skopeo --insecure-policy copy nix:${image} "$@"
+  '';
+
   loaderName =
     if pkgs.stdenv.hostPlatform.isx86_64 then
       "ld-linux-x86-64.so.2"
@@ -101,7 +175,7 @@ let
     ln -sfn ../libm.so.6 $out/lib/${libcDir}/libm.so.6
   '';
 
-  image = n2cLib.nix2container.buildImage {
+  rawImage = n2cLib.nix2container.buildImage {
     name = "opencode-container";
     tag = "latest";
     copyToRoot = imageRoot;
@@ -115,6 +189,19 @@ let
         "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
       ];
     };
+  };
+
+  image = rawImage // {
+    passthru = rawImage.passthru // {
+      copyToDockerDaemon = copyToDockerDaemon rawImage;
+      copyToRegistry = copyToRegistry rawImage;
+      copyToPodman = copyToPodman rawImage;
+      copyTo = copyTo rawImage;
+    };
+    copyToDockerDaemon = copyToDockerDaemon rawImage;
+    copyToRegistry = copyToRegistry rawImage;
+    copyToPodman = copyToPodman rawImage;
+    copyTo = copyTo rawImage;
   };
 in
 image // {
